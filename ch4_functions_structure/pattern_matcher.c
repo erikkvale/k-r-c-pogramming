00ef4210a7ebaf2f57f8d3adb131afb4e1ca914e
Erik Kvale <eirikval@gmail.com>
2018-09-29 21:45:13 -0500

    Filled out main method

--- ch4_functions_structure/pattern_matcher.c
+++ ch4_functions_structure/pattern_matcher.c
@@ -24,11 +24,21 @@ char pattern[] = "ould";
 
 // Find all lines matching pattern
 main() {
+  char line[MAXLINE];
+  int found = 0;
 
+  while (getline(line, MAXLINE) > 0) {
+    if (strindex(line, pattern) == 0) {
+      printf("%s\n", line);
+      found++''
+    }
+  }
+  return found;
 }
 
 // Get line into line[] array and return length of line[] array
 int getline(char line[], int max_char) {
+
   int c, i;
 
   i = 0;
@@ -44,6 +54,16 @@ int getline(char line[], int max_char) {
 
 // Return index of search pattern array in source line
 int strindex(char source[], char searchfor[]) {
+
   int i, j, k;
+
+  for (i = 0; source[i] != '\0'; i++) {
+    for (j = 1, k = 0; searchfor[k] != '\0' && source[k] == searchfor[k]; j++, k++) {
+      if (k > 0 && searchfor[k] == '\0') {
+        return i;
+      }
+    }
+  }
+  return -1;
 
 }
